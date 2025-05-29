13f18b9acfac016f148c827c1bf8c9cc30bff051
v1kah <victoria.khudz@gmail.com>
2025-05-29 22:57:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def recommend():
         return jsonify({"error": f"Token '{token}' not found in model."}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
